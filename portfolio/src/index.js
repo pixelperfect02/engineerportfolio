c3d1c5bbc32f24f0186135e56f051e25b29c2cc1
Archie Verma <a1745070@student.adelaide.edu.au>
2023-02-03 13:34:06 +1030

    Installed tailcss

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import 'react-mdl/extra/material.css';
+import 'react-mdl/extra/material.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
