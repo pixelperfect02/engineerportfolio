84b7fbd73d31a18c0ee2b2dfb91f169eb7325231
Archie Verma <archieverma114@gmail.com>
2025-02-10 22:01:59 +1030

    Finished odf

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -3,11 +3,15 @@ import React from "react";
 export default function Projects() {
   return (
     <section id="projects" className="text-gray-400 bg-white body-font">
-      <div className="projects-container">
+      <div className="projects-container flex justify-center items-center p-6">
         <iframe
-          src="https://online.flippingbook.com/view/790698976/"
+          src="https://e.issuu.com/embed.html?d=archievermasoftwareportfolio&u=archieverma"
           className="projects-3d-frame"
           title="Flipbook"
+          width="100%"
+          height="500px"
+          allowFullScreen
+          style={{ border: "none" }}
         ></iframe>
       </div>
     </section>
