e03d4c0392363a18b61d690223d2ff40478d7170
Archie Verma <archieverma114@gmail.com>
2025-02-11 14:07:23 +1030

    fixed flipbook size

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -6,16 +6,16 @@ const Projects = () => {
 
   return (
     <section id="projects" className="text-gray-400 bg-white body-font">
-      <div className="flex justify-center items-center p-6">
+      <div className="flipbook-container">
         <HTMLFlipBook
           width={900}
           height={600}
           showCover={true}
-          className="projects-3d-frame"
+          className="flipbook"
         >
           {[...Array(totalPages)].map((_, index) => (
-            <div key={index} className="page bg-white flex justify-center items-center">
-              <img src={`/page${index + 1}.png`} alt={`Page ${index + 1}`} className="w-full h-full" />
+            <div key={index} className="page">
+              <img src={`/page${index + 1}.png`} alt={`Page ${index + 1}`} />
             </div>
           ))}
         </HTMLFlipBook>
