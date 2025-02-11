c986bb962ee049ad1fa856734506b5fff8ebc4e9
Archie Verma <archieverma114@gmail.com>
2025-02-11 10:49:55 +1030

    added my own flipbook

--- portfolio/src/components/Projects.js
+++ portfolio/src/components/Projects.js
@@ -1,19 +1,27 @@
 import React from "react";
+import HTMLFlipBook from "react-pageflip";
 
-export default function Projects() {
+const Projects = () => {
+  const totalPages = 23;
+
   return (
     <section id="projects" className="text-gray-400 bg-white body-font">
-      <div className="projects-container flex justify-center items-center p-6">
-        <iframe
-          src="https://e.issuu.com/embed.html?d=archievermasoftwareportfolio&u=archieverma"
+      <div className="flex justify-center items-center p-6">
+        <HTMLFlipBook
+          width={900}
+          height={600}
+          showCover={true}
           className="projects-3d-frame"
-          title="Flipbook"
-          width="100%"
-          height="500px"
-          allowFullScreen
-          style={{ border: "none" }}
-        ></iframe>
+        >
+          {[...Array(totalPages)].map((_, index) => (
+            <div key={index} className="page bg-white flex justify-center items-center">
+              <img src={`/page${index + 1}.png`} alt={`Page ${index + 1}`} className="w-full h-full" />
+            </div>
+          ))}
+        </HTMLFlipBook>
       </div>
     </section>
   );
-}
+};
+
+export default Projects;
